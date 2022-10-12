fbb0f90638bef58db84839886f40a764689de32a
Godwin Udofia <udofiawin@gmail.com>
2022-10-12 20:32:56 +0100

    wip

---
+++ src/api/task.rs
@@ -0,0 +1,22 @@
+use actix_web::{
+    error::ResponseError,
+    get,
+    http::{header::ContentType, StatusCode},
+    post, put,
+    web::Data,
+    web::Json,
+    web::Path,
+    HttpResponse,
+};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct TaskIdentifier {
+    task_global_id: String,
+}
+
+#[get("/tasks/{task_global_id}")]
+pub async fn get_task(task_identifier: Path<TaskIdentifier>) -> Json<String> {
+    return Json(task_identifier.into_inner().task_global_id);
+}

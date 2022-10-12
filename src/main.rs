c43480437d82a0958492d075700c9a48fc1b0a3c
Godwin Udofia <udofiawin@gmail.com>
2022-10-12 20:50:35 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 mod api;
 
+use dotenv::dotenv;
+
 use api::task::get_task;
 
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     std::env::set_var("RUST_LOG", "debug");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
+
+    let port: String = std::env::var("PORT").expect("PORT not provided.");
 
     HttpServer::new(move || {
         let logger = Logger::default();
         App::new().wrap(logger).service(get_task)
     })
-    .bind(("127.0.0.1", 3200))?
+    .bind(("127.0.0.1", port.parse::<u16>().unwrap()))?
     .run()
     .await
 }
